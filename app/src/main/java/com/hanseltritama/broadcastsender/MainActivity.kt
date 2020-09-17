cfd57cf26e2bfb15ec331bab90d5b8e02d56ffb2
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-17 01:46:44 -0700

    added sender receiver

--- app/src/main/java/com/hanseltritama/broadcastsender/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/broadcastsender/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.hanseltritama.broadcastsender
 
 import android.content.*
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,30 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     fun customSendBroadcast(view: View) {
         val intent = Intent("com.hanseltritama.MY_ACTION")
-        // 1st approach
-//        val cn = ComponentName("com.hanseltritama.broadcastreceiver",
-//            "com.hanseltritama.broadcastreceiver.MyBroadcastReceiver")
-//        intent.component = cn
+        intent.setPackage("com.hanseltritama.broadcastreceiver")
 
-        // 2nd approach
-//        intent.setClassName("com.hanseltritama.broadcastreceiver",
-//            "com.hanseltritama.broadcastreceiver.MyBroadcastReceiver")
+        val extras = Bundle()
+        extras.putString("stringExtra", "Start")
 
-        // 3rd approach
-//        intent.setPackage("com.hanseltritama.broadcastreceiver")
-
-        // 4th approach
-        val packageManager: PackageManager = packageManager
-
-        // check which app that has a BroadcastReceiver that is registered
-        // with an intent-filter com.hansel.MY_ACTION
-        // Let the system found its receiver
-        val infos: List<ResolveInfo> = packageManager.queryBroadcastReceivers(intent, 0)
-        for (info in infos) {
-            val cn = ComponentName(info.activityInfo.packageName,
-                info.activityInfo.name)
-            intent.component = cn
-            sendBroadcast(intent)
-        }
+        sendOrderedBroadcast(intent,
+            null,
+            SenderReceiver(),
+            null,
+            0,
+            "Start",
+            extras)
     }
 }
