72ef495d2ee5d62c2f4de67ee39f89e5f2735b94
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-09-17 01:02:29 -0700

    added broadcast sender

--- app/src/main/java/com/hanseltritama/broadcastsender/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/broadcastsender/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.hanseltritama.broadcastsender
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import android.content.*
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -17,26 +16,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun customSendBroadcast(view: View) {
-        val intent: Intent = Intent("com.hanseltritama.MY_ACTION")
-        intent.putExtra("com.hanseltritama.MY_TEXT", "Broadcast Received")
-        sendBroadcast(intent)
-    }
+        val intent = Intent("com.hanseltritama.MY_ACTION")
+        // 1st approach
+//        val cn = ComponentName("com.hanseltritama.broadcastreceiver",
+//            "com.hanseltritama.broadcastreceiver.MyBroadcastReceiver")
+//        intent.component = cn
 
-    private val broadcastReceiver: BroadcastReceiver = object: BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val receivedText: String? = intent?.getStringExtra("com.hanseltritama.MY_TEXT")
-            text_view.text = receivedText
+        // 2nd approach
+//        intent.setClassName("com.hanseltritama.broadcastreceiver",
+//            "com.hanseltritama.broadcastreceiver.MyBroadcastReceiver")
+
+        // 3rd approach
+//        intent.setPackage("com.hanseltritama.broadcastreceiver")
+
+        // 4th approach
+        val packageManager: PackageManager = packageManager
+
+        // check which app that has a BroadcastReceiver that is registered
+        // with an intent-filter com.hansel.MY_ACTION
+        // Let the system found its receiver
+        val infos: List<ResolveInfo> = packageManager.queryBroadcastReceivers(intent, 0)
+        for (info in infos) {
+            val cn = ComponentName(info.activityInfo.packageName,
+                info.activityInfo.name)
+            intent.component = cn
+            sendBroadcast(intent)
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val intentFilter: IntentFilter = IntentFilter("com.hanseltritama.MY_ACTION")
-        registerReceiver(broadcastReceiver, intentFilter)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        unregisterReceiver(broadcastReceiver)
     }
 }
